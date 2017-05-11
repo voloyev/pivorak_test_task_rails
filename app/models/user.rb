319b1cd66bf23647e225288789bb11705e9a8318
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-11 15:17:38 +0300

    Add password valdation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name,  presence: true
   validates :email, presence: true, uniqueness: true
-
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end
