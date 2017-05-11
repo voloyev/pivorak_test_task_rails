250a49c0a9ae348b61d96d4d27ea7608fd2c2054
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-11 15:02:21 +0300

    Add index to email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505122025) do
+ActiveRecord::Schema.define(version: 20170511115051) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170505122025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
