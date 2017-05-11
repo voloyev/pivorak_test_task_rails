918a34f0d901ac7c2febca5eebc450e5a33b4ee8
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-11 15:04:37 +0300

    Add secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511115051) do
+ActiveRecord::Schema.define(version: 20170511120352) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.boolean  "admin"
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
