5d70d0be1c7338cb32e0ab3ab5519ba1c3798ccb
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-13 00:51:50 +0300

    Add create and index action

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,2 +1,33 @@
 class BusesController < ApplicationController
+
+  def new
+    @bus = Bus.new
+  end
+
+  def create
+    @bus = Bus.new(bus_params)
+    if @bus.save
+      redirect_to root_path
+      flash[:succes] = 'Bus added'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @buses = Bus.all
+  end
+
+  private
+
+  def bus_params
+    params.require(:bus).permit(:name, :from, :to, :date_from, :date_to, :time_from, :time_to)
+  end
+
+  def set_bus
+    @bus = Bus.find(params[:id])
+  end
 end
