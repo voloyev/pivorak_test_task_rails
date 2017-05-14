5fa37bb34c48b334864773a1e945a441fcb8b699
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-14 14:09:02 +0300

    Generate model for tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514074953) do
+ActiveRecord::Schema.define(version: 20170514110844) do
 
   create_table "buses", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 20170514074953) do
     t.datetime "date_and_time_from"
     t.datetime "date_and_time_to"
     t.integer  "seats"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
