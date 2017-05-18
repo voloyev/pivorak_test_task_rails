83a7e4fae119c243c2403296245c736158532aff
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-19 00:14:36 +0300

    Revisit

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,19 +1,14 @@
 class Bus < ApplicationRecord
   has_and_belongs_to_many :bus_stops
-<<<<<<< HEAD
-
   has_many :tickets
+
   before_save :set_name
-=======
-  before_save :name
->>>>>>> 5b7e124... Revisit tables structure; add a little bit validations
 
   validates :from, presence: true
   validates :to, presence: true
 
   protected
 
-<<<<<<< HEAD
   def set_name
     self.name = self.from.to_s + '-' + self.to.to_s
   end
@@ -24,9 +19,4 @@ class Bus < ApplicationRecord
       self.tickets << Ticket.create(seat: i, bus_id: self.id)
     end
   end
-=======
-  def name
-    self.name = self.from.to_s + '-' + self.to.to_s
-  end
->>>>>>> 5b7e124... Revisit tables structure; add a little bit validations
 end
