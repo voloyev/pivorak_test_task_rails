4fcfc320f9a0d8ca579af6f03814ebfeb3f3e96f
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-18 23:53:39 +0300

    Add has_many tickets to bus

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,13 +1,22 @@
 class Bus < ApplicationRecord
   has_and_belongs_to_many :bus_stops
-  before_save :name
+
+  has_many :tickets
+  before_save :set_name
 
   validates :from, presence: true
   validates :to, presence: true
 
   protected
 
-  def name
+  def set_name
     self.name = self.from.to_s + '-' + self.to.to_s
+  end
+
+  def create_tickets
+    self.seats.times do |i = 0|
+      i += 1
+      self.tickets << Ticket.create(seat: i, bus_id: self.id)
+    end
   end
 end
