7996be76b070eb6052c7f1eb9f47d68e2fb35f67
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-19 22:27:45 +0300

    Remove indexes

--- db/migrate/20170519111339_create_bus_stops_buses.rb
+++ db/migrate/20170519111339_create_bus_stops_buses.rb
@@ -3,9 +3,6 @@ class CreateBusStopsBuses < ActiveRecord::Migration[5.0]
     create_table :bus_stops_buses do |t|
       t.integer :bus_id
       t.integer :bus_stop_id
-
-      add_index :bus_stops, :bus_id
-      add_index :buses, :bus_stop_id
     end
   end
 end
