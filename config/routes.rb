3918e55f91b4224a89029c4ee50e61a7ea145ea2
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-19 12:45:18 +0300

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # resources :users
+  resources :users, only: :show
+  post 'users/buy_ticket', to: 'users#buy_ticket', as: 'buy_ticket'
 
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
   end
 
-  resources :buses
-  resources :tickets
-  
+  resources :buses do
+    resources :tickets
+  end
+
   root 'buses#index'
 end
